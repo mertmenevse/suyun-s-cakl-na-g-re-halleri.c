8cb475a10d99ef28c8c495033f32e78c6b474ba7
Mehmet Mert Menevşe <mert.menevse32@gmail.com>
2021-02-08 14:04:28 +0300

    ikl commit

---
+++ suyunhalleri.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	
+	int sicaklik;
+	
+	printf("Suyun sicakligini giriniz=");
+	scanf("%d",&sicaklik);
+	
+	if(sicaklik<0){
+		printf("Suyun sicakligi %d ve kati haldedir.",sicaklik);
+	}
+	else if(0<sicaklik<100){
+		printf("Suyun sicakligi %d ve sivi haldedir.",sicaklik);
+	}
+	else{
+		printf("Suyun sicakligi %d ve gaz haldedir.",sicaklik);
+	}
+		
+	
+	
+	
+	
+	return 0;
+}
